00b7897f15327889f836555a0d6fc50aeae2c48e
scheidan <andreas.scheidegger@eawag.ch>
2023-03-31 14:31:29 +0200

    add some R and julia intro

--- src/build_html.r
+++ src/build_html.r
@@ -19,7 +19,7 @@ src.dir <- "src"
 
 ## --- compile all exercises
 exercises <- c("exercise_0", "exercise_1", "exercise_2",
-               "exercise_3", "exercise_4", "exercise_5")[4]
+               "exercise_3", "exercise_4", "exercise_5")[0]
 
 exercises <- c(exercises, "index")
 for(exer in exercises){
